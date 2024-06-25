50f87faf08f8d665f1ca98524aa69c5597b2e28a
TheBigBlob <88168493+TheBigBlob@users.noreply.github.com>
2024-06-24 23:25:26 -0700

    Initial commit

---
+++ src/main/java/net/landen/mccourse/MCCourseMod.java
@@ -0,0 +1,16 @@
+package net.landen.mccourse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCourseMod implements ModInitializer {
+	public static final String MOD_ID = "mccourse";
+    public static final Logger LOGGER = LoggerFactory.getLogger("mccourse");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/landen/mccourse/MCCourseModClient.java
@@ -0,0 +1,9 @@
+package net.landen.mccourse;
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
