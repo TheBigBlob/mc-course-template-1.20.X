489fdcc1a732d5679654fd533183b837e0811a4a
TheBigBlob <88168493+TheBigBlob@users.noreply.github.com>
2024-06-25 22:09:56 -0700

    Added comments for how everything works within the ModItems class

--- src/main/java/net/landen/mccourse/item/ModItems.java
+++ src/main/java/net/landen/mccourse/item/ModItems.java
@@ -12,16 +12,22 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    // "=" is initializing the object,
     public static final Item PINK_GARNET = registerItem("pink_garnet",
             new Item(new FabricItemSettings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
             new Item(new FabricItemSettings()));
 
-
+    // cleans up code through registering item more efficiently
     private static Item registerItem(String name, Item item) {
+        // accessing register class with Registry, .register is accessing a method within the Registry class, () indicate parameters of register
+        // .ITEM is a variable within the class Registries; it is all caps because it is a static final variable
+        // new Identifier is creating a new "Identifier" object
+        // Registry.register method takes in a Registry, identifier, and entry; returns type ITEM due to Registries.ITEM, Identifier gives its custom id in the game
         return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
     }
 
+    // adds objects to a list
     private static void itemGroupIngredients(FabricItemGroupEntries entries) {
         entries.add(PINK_GARNET);
         entries.add(RAW_PINK_GARNET);
@@ -30,6 +36,7 @@ public class ModItems {
         entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
     }
 
+    // adds the list of entries in itemGroupIngredients to the creative tab ingredients
     public static void registerModItems() {
         MCCourseMod.LOGGER.info("Registering Mod Items for " + MCCourseMod.MOD_ID);
 
