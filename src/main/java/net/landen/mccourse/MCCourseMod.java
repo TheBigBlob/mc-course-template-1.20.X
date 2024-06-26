87b1f91364c8004f29a35c0bc4294514eb4988e6
TheBigBlob <88168493+TheBigBlob@users.noreply.github.com>
2024-06-25 18:55:21 -0700

    Added ItemGroup "Pink Garnet Course Group"

--- src/main/java/net/landen/mccourse/MCCourseMod.java
+++ src/main/java/net/landen/mccourse/MCCourseMod.java
@@ -3,6 +3,7 @@ package net.landen.mccourse;
 import net.fabricmc.api.ModInitializer;
 
 import net.landen.mccourse.block.ModBlocks;
+import net.landen.mccourse.item.ModItemGroup;
 import net.landen.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroup.registerItemGroups();
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
